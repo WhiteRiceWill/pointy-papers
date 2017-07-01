abd70935a8b578d873f61287c43832aac504939e
WhiteRiceWill <wmchristman123@gmail.com>
2017-07-01 00:52:26 -0700

    implement timer to remove thumb emoji and adjust several asthetic issues

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Download from '../Download/Download.js';
 import TestPic from '../../Assets/Paper4/PP4-4K.png';
-
 
 var title1A = "5K";
 var title1B = "(5120 x 2880)";
@@ -37,95 +36,32 @@ var desc5 = "Also fits 1280x720";
 var desc6 = "Also fits 2224x2224 and 2048x2048";
 var desc7 = "Also fits 750x1334";
 
-export default class Card extends Component{
+export default class Card extends Component {
   render() {
     return (
-    <div className="card">
-    <img src={TestPic} alt="Test" className="cardimg"/>
-    <div className="downloadHeader">
-    <p>Download</p>
-    </div>
+      <div className="card">
+        <img src={TestPic} alt="Test" className="cardimg"/>
+        <div className="downloadHeader">
+          <p>Download</p>
+        </div>
 
-      <div className="downloadColumn1">
-        <Download
-          titleA={title1A}
-          titleB={title1B}
-          hasDesc={false}
-          imgNum={this.props.imgNum}
-          imgType="5K"
-        />
-        <Download
-          titleA={title2A}
-          titleB={title2B}
-          hasDesc={true}
-          description={desc2}
-          imgNum={this.props.imgNum}
-          imgType="4K"
-        />
-        <Download
-          titleA={title3A}
-          titleB={title3B}
-          hasDesc={true}
-          description={desc3}
-          imgNum={this.props.imgNum}
-          imgType="UltraWide"
-        />
-        <Download
-          titleA={title4A}
-          titleB={title4B}
-          hasDesc={true}
-          description={desc4}
-          imgNum={this.props.imgNum}
-          imgType="MacBook"
-        />
-        <Download
-          titleA={title5A}
-          titleB={title5B}
-          hasDesc={true}
-          description={desc5}
-          imgNum={this.props.imgNum}
-          imgType="HD"
-        />
-    </div>
+        <div className="downloadColumn1">
+          <Download titleA={title1A} titleB={title1B} hasDesc={false} imgNum={this.props.imgNum} imgType="5K"/>
+          <Download titleA={title2A} titleB={title2B} hasDesc={true} description={desc2} imgNum={this.props.imgNum} imgType="4K"/>
+          <Download titleA={title3A} titleB={title3B} hasDesc={true} description={desc3} imgNum={this.props.imgNum} imgType="UltraWide"/>
+          <Download titleA={title4A} titleB={title4B} hasDesc={true} description={desc4} imgNum={this.props.imgNum} imgType="MacBook"/>
+          <Download titleA={title5A} titleB={title5B} hasDesc={true} description={desc5} imgNum={this.props.imgNum} imgType="HD"/>
+        </div>
 
-    <div className="downloadColumn2">
-      <Download
-        titleA={title6A}
-        titleB={title6B}
-        hasDesc={true}
-        description={desc6}
-        imgNum={this.props.imgNum}
-        imgType="Tablet"
-      />
-      <Download
-        titleA={title7A}
-        titleB={title7B}
-        hasDesc={true}
-        description={desc7}
-        imgNum={this.props.imgNum}
-        imgType="Smartphone"
-      />
-      <Download
-        titleA={title8A}
-        titleB={title8B}
-        hasDesc={false}
-        imgNum={this.props.imgNum}
-        imgType="GalaxyS8"
-      />
-      <Download
-        titleA={title9A}
-        titleB={title9B}
-        hasDesc={false}
-        imgNum={this.props.imgNum}
-        imgType="Twitter"
-      />
+        <div className="downloadColumn2">
+          <Download titleA={title6A} titleB={title6B} hasDesc={true} description={desc6} imgNum={this.props.imgNum} imgType="Tablet"/>
+          <Download titleA={title7A} titleB={title7B} hasDesc={true} description={desc7} imgNum={this.props.imgNum} imgType="Smartphone"/>
+          <Download titleA={title8A} titleB={title8B} hasDesc={false} imgNum={this.props.imgNum} imgType="GalaxyS8"/>
+          <Download titleA={title9A} titleB={title9B} hasDesc={false} imgNum={this.props.imgNum} imgType="Twitter"/>
 
-  </div>
+        </div>
 
-
-
-
-    </div>
+      </div>
     );
   }
 };
--- src/Components/Download/Download.js
+++ src/Components/Download/Download.js
@@ -1,87 +1,74 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TestPic from '../../Assets/Other/thumbEmoji.png';
 
-
-
-export default class Download extends Component{
+export default class Download extends Component {
   constructor() {
     super();
-    this.state = {isToggleOn: false, currentCount: 5};
+    this.state = {
+      isToggleOn: false,
+      currentCount: 2
+    };
     this.handleClick = this.handleClick.bind(this);
-    this.toggleState = this.toggleState.bind(this);
     this.timerGo = this.timerGo.bind(this);
   }
 
   handleClick() {
-  this.setState(prevState => ({
-    isToggleOn: !prevState.isToggleOn
-  }));
-  var countdown = setInterval(this.timerGo, 1000);
-  this.setState({intervalId: countdown});
-}
-
-toggleState(){
-  this.setState(prevState => ({
-    isToggleOn: !prevState.isToggleOn
-  }));
-}
-
-
-
-componentWillUnmount() {
-   clearInterval(this.state.countdown);
-}
-
-timerGo() {
-  if(this.state.isToggleOn){
-   this.setState({ currentCount: this.state.currentCount -1 });
- }
-}
-
-resetTimer() {
-  this.setState({currentCount: 5});
-}
-
-componentWillUpdate(){
-  if(this.state.currentCount < 1){
     this.setState(prevState => ({
       isToggleOn: !prevState.isToggleOn
     }));
-    this.resetTimer();
+
+    var countdown = setInterval(this.timerGo, 1000);
+    this.setState({intervalId: countdown});
   }
 
+  componentWillUpdate() {
+    if (this.state.currentCount < 1) {
+      this.setState(prevState => ({isToggleOn: false}));
+      this.resetTimer();
+      clearInterval(this.state.intervalId);
+    }
+  }
 
-}
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
 
+  timerGo() {
+    if (this.state.isToggleOn) {
+      this.setState({
+        currentCount: this.state.currentCount - 1
+      });
+    }
+  }
+
+  resetTimer() {
+    this.setState({currentCount: 2});
+  }
 
 
 
   render() {
-    console.log(this.state.currentCount);
-    console.log(this.state.isToggleOn);
     var desc = this.props.description;
     const imgName = require('../../Assets/Paper' + this.props.imgNum + '/PP' + this.props.imgNum + '-' + this.props.imgType + '.png');
 
-
-
-    if(this.state.isToggleOn){
+    if (this.state.isToggleOn) {
       return (
         <div className="downloadBox">
-          <div className= "tooltip">
-              <button  className="downloadBtn">
-                  <img src={TestPic} alt="Test" className="thumbImg"/>
-              </button>
+          <div className="tooltip">
+            <button className="downloadBtnHolder">
+              <img src={TestPic} alt="Test" className="thumbImg"/>
+            </button>
           </div>
         </div>
       );
-    }
-    else if(this.props.hasDesc){
+    } else if (this.props.hasDesc) {
       return (
         <div className="downloadBox">
           <div className="tooltip">
             <a href={imgName} download>
               <button type="button" className="downloadBtn" onClick={this.handleClick}>
-                {this.props.titleA} <br/> {this.props.titleB}
+                {this.props.titleA}
+                <br/> {this.props.titleB}
               </button>
             </a>
             <span className="tooltipText">{desc}</span>
@@ -89,19 +76,18 @@ componentWillUpdate(){
         </div>
 
       );
-    }
-    else {
+    } else {
       return (
         <div className="downloadBox">
-          <div className= "tooltip">
+          <div className="tooltip">
             <a href={imgName} download>
               <button type="button" className="downloadBtn" onClick={this.handleClick}>
-                {this.props.titleA} <br/> {this.props.titleB}
+                {this.props.titleA}
+                <br/> {this.props.titleB}
               </button>
             </a>
           </div>
         </div>
-
 
       );
     }
