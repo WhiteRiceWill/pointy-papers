493b6c0c31c94f0dac4a87521a9743650a27c2c5
WhiteRiceWill <wmchristman123@gmail.com>
2017-07-13 20:28:17 -0700

    add welcome component

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import Card from '../Card/Card.js';
+import Welcome from '../Welcome/Welcome.js';
 
 export default class Feed extends Component{
   render() {
     return (
       <div className="feed">
       <div className="feedLeft">
+        <Welcome/>
         <Card imgNum="4"/>
         <Card imgNum="1"/>
         <Card imgNum="7"/>
         <Card imgNum="5"/>
       </div>
       <div className="feedRight">
+
         <Card imgNum="2"/>
         <Card imgNum="6"/>
         <Card imgNum="3"/>
---
+++ src/Components/Welcome/Welcome.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Welcome extends Component{
+  render() {
+    return (
+      <div className="welcomeCard">
+        <div className="welcomeText">
+          <div>
+            Pointy Papers is a collection of
+          </div>
+          <div>
+            <Link to='/license' className='freeLink'>free </Link>
+
+           to download,
+          </div>
+          <div>
+            minimalist wallpapers.
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+};
